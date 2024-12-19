76c1549f40f907b6c53b97f9226beaabe05a86fb
linzheng <alidlg9045@126.com>
2024-12-19 15:37:52 +0800

    feat: install clerk to do auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from '@clerk/nextjs'
+
 export default function Home() {
   return (
     <div>
       <h1>Next Ecommerce Admin Dashboard</h1>
+      <UserButton />
     </div>
   );
 }
