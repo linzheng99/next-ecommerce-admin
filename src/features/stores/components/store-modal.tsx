21bcb5bf53ad81bf53cce42d2c3ede5f575a76cb
linzheng <alidlg9045@126.com>
2024-12-19 21:32:40 +0800

    feat: add store modal

--- src/features/stores/components/store-modal.tsx
+++ src/features/stores/components/store-modal.tsx
@@ -1,11 +1,40 @@
 "use client"
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod"
+
 import Modal from "@/components/modal";
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
 import { useStoreModal } from "../store/use-store-modal";
 
 export default function StoreModal() {
   const storeModal = useStoreModal()
+
+  const formSchema = z.object({
+    name: z.string().min(1, { message: "Store name is required" }),
+  })
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
 
   return (
     <Modal
@@ -14,10 +43,28 @@ export default function StoreModal() {
       title="Create store"
       description="Add a new store to manage products and categories"
     >
-      <div>
-        <div className="space-y-4 py-2 pb-4">
-          children
-        </div>
+      <div className="w-full">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-4 w-full">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel> Name </FormLabel>
+                  <FormControl>
+                    <Input placeholder="E-Commerce Store" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center justify-end gap-4 w-full">
+              <Button type="button" variant="outline" onClick={storeModal.onClose}>Cancel</Button>
+              <Button type="submit">Continue</Button>
+            </div>
+          </form>
+        </Form>
       </div>
     </Modal>
   )
